80f8486703fab941d4351ab06b5e58e4e522d50f
William <billycraig2009@hotmail.com>
2019-02-19 11:25:20 -0600

    add animals sln with console app.

---
+++ animals/animals.ui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace animals.ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
